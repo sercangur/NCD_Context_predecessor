4dc7203f14b5e1ddba8b113038ed9052dffb238f
fightlikeagirl <v.aysu@hotmail.com>
2022-04-11 23:34:31 +0300

    first commit

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,8 +1,12 @@
 import { storage, Context } from "near-sdk-as"
 
 // return the string 'hello world'
-export function helloWorld(): string {
-  return 'hello world'
+export function helloWorld(names: Array<string>): string {
+  return names.map<string>(name => 'hello ' + name).join(` `)
+}
+
+export function helloSercan(): string {
+  return 'hello Sercan'
 }
 
 // read the given key from account (contract) storage
